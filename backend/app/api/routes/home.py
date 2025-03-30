eb96a71055cd515087f0005367fa47096e8aa84f
API-Imperfect <alphaogilo@gmail.com>
2025-03-30 11:56:14 +0200

    feat: add logging with loguru

--- backend/app/api/routes/home.py
+++ backend/app/api/routes/home.py
@@ -1,8 +1,16 @@
+from app.core.logging import get_logger
 from fastapi import APIRouter
+
+logger = get_logger()
 
 router = APIRouter(prefix="/home")
 
 
 @router.get("/")
 def home():
+    logger.info("Home page acccessed")
+    logger.debug("Home page acccessed")
+    logger.error("Home page acccessed")
+    logger.warning("Home page acccessed")
+    logger.critical("Home page acccessed")
     return {"message": "Welcome to the NextGen Bank API"}
