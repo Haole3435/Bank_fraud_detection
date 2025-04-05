dc21d164bf056c3553dbd6ba98aab2509385bdff
API-Imperfect <alphaogilo@gmail.com>
2025-04-05 20:26:07 +0200

    chore: install celery and email related packages

--- backend/app/api/routes/home.py
+++ backend/app/api/routes/home.py
@@ -9,9 +9,4 @@ router = APIRouter(prefix="/home")
 
 @router.get("/")
 def home():
-    logger.info("Home page acccessed")
-    logger.debug("Home page acccessed")
-    logger.error("Home page acccessed")
-    logger.warning("Home page acccessed")
-    logger.critical("Home page acccessed")
     return {"message": "Welcome to the NextGen Bank API"}
