821f97e21024f9c1f5a380553e47f9580b2f731f
API-Imperfect <alphaogilo@gmail.com>
2025-05-23 15:07:36 +0200

    chore: add bank account enums,schema and models

--- backend/app/auth/models.py
+++ backend/app/auth/models.py
@@ -10,6 +10,7 @@ from sqlmodel import Column, Field, Relationship
 from backend.app.auth.schema import BaseUserSchema, RoleChoicesSchema
 
 if TYPE_CHECKING:
+    from backend.app.bank_account.models import BankAccount
     from backend.app.next_of_kin.models import NextOfKin
     from backend.app.user_profile.models import Profile
 
@@ -56,6 +57,8 @@ class User(BaseUserSchema, table=True):
         },
     )
     next_of_kins: list["NextOfKin"] = Relationship(back_populates="user")
+
+    bank_accounts: list["BankAccount"] = Relationship(back_populates="user")
 
     @computed_field
     @property
