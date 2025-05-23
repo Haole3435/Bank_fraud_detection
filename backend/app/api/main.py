994bb06c4fdc20e0be867a48d3b27bfa507b4ac9
API-Imperfect <alphaogilo@gmail.com>
2025-05-23 14:27:15 +0200

    feat: user can delete their next of kin

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -11,6 +11,7 @@ from backend.app.api.routes.auth import (
 )
 from backend.app.api.routes.next_of_kin import all
 from backend.app.api.routes.next_of_kin import create as create_next_of_kin
+from backend.app.api.routes.next_of_kin import delete
 from backend.app.api.routes.next_of_kin import update as update_next_of_kin
 from backend.app.api.routes.profile import all_profiles, create, me, update, upload
 
@@ -31,3 +32,4 @@ api_router.include_router(all_profiles.router)
 api_router.include_router(create_next_of_kin.router)
 api_router.include_router(all.router)
 api_router.include_router(update_next_of_kin.router)
+api_router.include_router(delete.router)
